274c2cd330d5cc0437f27b366e535c7eef4c18b2
gitkavindu <hansana2024@gmail.com>
2024-12-03 22:24:25 +0530

    Load 8 CSV files into a list

--- scripts/load_and_combine.py
+++ scripts/load_and_combine.py
@@ -1,5 +1,30 @@
 import pandas as pd
+import os
 
-df = pd.read_csv('data/raw/Australia_Wine_Stats.csv')
-print(df.head()) 
-#need to complete this
+data_folder = "./data/raw/"
+
+# get all csv files path to a single list
+csv_files = [f for f in os.listdir(data_folder) if f.endswith('.csv')]
+
+# Load all CSV files into DataFrames and store them in a list
+data_list = []
+for file in csv_files:
+    file_path = os.path.join(data_folder, file)
+    df = pd.read_csv(file_path)
+    data_list.append(df)
+
+print(data_list[0])
+
+
+
+
+# dataframes = []
+# for file in csv_files:
+#     file_path = os.path.join(data_folder, file)
+#     df = pd.read_csv(file_path)
+#     dataframes.append(df)  
+
+# print(dataframes)
+
+# print("details")
+# print(dataframes["Name"])
