6edb07266f2c412587d42866c6d0c1265ad480fc
SulakshiChathurika <whscw28@gmail.com>
2024-12-03 23:37:23 +0530

    create country and country_region columns

--- scripts/load_and_combine.py
+++ scripts/load_and_combine.py
@@ -15,6 +15,16 @@ for file in csv_files:
 
 # Concatenate the files into a single DataFrame
 wine_df = pd.concat(data_list, ignore_index=True)
+print(wine_df)
+
+# Split the 'Region' column by '/' and create new columns
+wine_df['Country'] = wine_df['Region'].str.split('/').str[0].str.strip()  # First part as 'Country'
+wine_df['Country_Region'] = wine_df['Region'].str.split('/').str[1].str.strip()  # Second part as 'Country_Region'
+
+# Display the first few rows to confirm the changes
+print(wine_df[['Region', 'Country', 'Country_Region']].head())
+print(wine_df)
+
 
 
 
