7a575b0f6a2b834b1c98505b9614ecd80d5d5d9b
SulakshiChathurika <whscw28@gmail.com>
2024-12-04 14:04:08 +0530

    Data cleaning

--- scripts/load_and_combine.py
+++ scripts/load_and_combine.py
@@ -58,7 +58,7 @@ wine_df.info()
 
 
 # Create a new column with row numbers starting from 1
-wine_df.insert(0, "row number", range(1, len(wine_df) + 1))
+wine_df.insert(0, "Index", range(1, len(wine_df) + 1))
 wine_df.drop(columns=['Unnamed: 0'])
 
 # Display the updated DataFrame
@@ -81,7 +81,7 @@ def find_percentile_outliers(df, columns, lower_percentile=1, upper_percentile=9
         outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)].index
         
         # Get the "row number" values for the outliers
-        outlier_row_numbers = df.loc[outliers, 'row number'].tolist()
+        outlier_row_numbers = df.loc[outliers, 'Index'].tolist()
         
         # Store the row numbers for outliers in a dictionary
         outlier_rows[column] = outlier_row_numbers
@@ -89,7 +89,7 @@ def find_percentile_outliers(df, columns, lower_percentile=1, upper_percentile=9
     return outlier_rows
 
 # Columns to check for outliers
-columns_to_check = ['Rating', 'Price', 'Alcohol content', 'Bold', 'Tannin', 'Sweet', 'Acidic']
+columns_to_check = ['Rating', 'Price', 'Bold', 'Tannin', 'Sweet', 'Acidic']
 
 # Get the rows with outliers outside the 1st and 99th percentiles
 outlier_rows = find_percentile_outliers(wine_df, columns_to_check, lower_percentile=1, upper_percentile=99)
@@ -99,6 +99,21 @@ for column, rows in outlier_rows.items():
     print(f"Outliers for column '{column}':")
     print(rows)
     print("\n")
+
+# the 'Rating' and 'Price' columns show values outside the 1st and 99th percentiles, it is important to recognize that these 
+# values might still be valid and meaningful within the context of the dataset.
+# There are no absolute "limits" for the values of Tannin, Sweet, Acidic, and Bold in wines, as these attributes can vary widely 
+# depending on the grape variety, wine-making process, and style of wine.
+print("Interpretation:")
+print("Outliers in the 'Rating,' 'Price,' 'Tannin,' 'Sweet,' 'Acidic,' and 'Bold' columns in wines are considered valid and should not be" 
+      "removed due to domain-specific reasons. These outliers may represent rare but legitimate cases, such as premium products or exceptional"
+      " ratings, which are important for accurately capturing the full range of wine characteristics.")
+
+# Get rows where alcohol content is greater than 16 or less than 9
+non_regular_wines = wine_df[(wine_df['Alcohol content'] > 16) | (wine_df['Alcohol content'] < 9)]
+print(non_regular_wines)
+print("Wines below 9% alcohol are low-alcohol wines, like Moscato or low-alcohol Riesling. Wines above 16% are high-alcohol, typically fortified wines like Port or Sherry.")
+
 
 
 
